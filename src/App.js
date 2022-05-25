b18e4a55e9ed059515a91ef36a7c379dc3eddfb6
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-05-25 11:14:53 +0200

    feat: Props-01
    also added proper code to index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Hello from "./Hello";
+import Welcome from "./Welcome"
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Hello />
+                <Welcome name={"John"} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App />);
