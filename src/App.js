c124b6fbfcecf7a7d373b0c973fff3cbb872390e
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-05-25 10:43:55 +0200

    feat: JSX-03
    also renamed Foo from JSX-02 into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-let hello = <h1>Hello World!</h1>;
+let hello = <h1>Hello World</h1>;
 
-function Foo(name) {
-    return <h1>Hello, {name}</h1>;
+function App(name) {
+    return <h1>Hello {name}</h1>;
 }
+
+export default App;
+
+/* JSX-03
+    Q: What happens if the name variable is not passed to the function?
+    A: The App function would not render the "name";
+*/
