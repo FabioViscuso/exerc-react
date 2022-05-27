f32f01aa02f080aa1a1f8f64220c648afa322fe7
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-05-27 19:48:44 +0200

    feat: State-01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Counter from "./Counter";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 
@@ -6,6 +7,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <Counter />
                 <Hello />
                 {/* Within curly braces we can pass any valid JS/JSX expression */}
                 <Welcome name={"John"} age={29} />
