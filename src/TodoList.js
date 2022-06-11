904ee2605603914acf46e060785a93f6a0597ae9
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-06-12 01:40:19 +0200

    feat: bonus: better functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,25 +2,29 @@ import React from "react";
 
 class TodoList extends React.Component {
     state = {
-        items: []
+        todos: []
     }
 
     addToTodoHandler = (event) => {
         event.preventDefault();
-        this.setState({ items: [...this.state.items, event.target.elements.newTodo.value] })
+        this.setState({ todos: [...this.state.todos, event.target.elements.newTodo.value] });
         event.target.reset();
-        document.querySelector('#todoAdder').disabled = true
+        document.querySelector('#todoAdder').disabled = true;
     }
 
     deleteTodoHandler = (event) => {
-        const target = event.target.previousSibling.innerText;
+        const listNumber = Number(event.target.parentElement.attributes.todonumber.value);
         this.setState((state) => {
-            return { items: state.items.filter((item) => item !== target) }
+            return {
+                todos: state.todos.filter((item, index) => {
+                    return index !== listNumber
+                })
+            }
         })
     }
 
     clearTodosHandler = () => {
-        this.setState({ items: [] });
+        this.setState({ todos: [] });
     }
 
     emptyInputChecker = () => {
@@ -42,12 +46,13 @@ class TodoList extends React.Component {
                 <ul>
                     {
                         // eslint-disable-next-line array-callback-return
-                        Object.values(this.state.items).map((item, index) => {
+                        this.state.todos.map((item, index) => {
                             if (item !== "") {
-                                return (<>
-                                    <li id={'todonumber' + index} key={index}>{item}</li>
-                                    <button onClick={this.deleteTodoHandler}>&nbsp; Delete</button>
-                                </>)
+                                return (
+                                    <li todonumber={index} key={'list' + index}>{item}&nbsp;
+                                        <button key={'btn' + index} onClick={this.deleteTodoHandler}>Delete</button>
+                                    </li>
+                                )
                             }
                         })
                     }
