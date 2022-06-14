d1e2a6f9e9eb7e6fec28ed821fbd0cd5d05f90b4
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-06-14 09:32:03 +0200

    feat: Render Props-01
    NOTE: the exercise has been substantially modified for educational purposes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import InteractiveWelcome from "./InteractiveWelcome";
-import TodoList from "./TodoList";
-import Login from "./Login";
-import Container from "./Container";
+/* import InteractiveWelcome from "./InteractiveWelcome"; */
+import ConfigurableList from "./ConfigurableList";
+/* import Login from "./Login"; */
+/* import Container from "./Container"; */
 /* import UncontrolledLogin from "./UncontrolledLogin"; */
 
 function App() {
-    return (<div>
-        <InteractiveWelcome />
-        <Login />
-        {/* <UncontrolledLogin /> */}
-        <TodoList />
-        <Container title={<h1>Hello world!</h1>}>
+    const catStyle = {
+        width: '200px',
+        borderRadius: '10px'
+    }
+    return (
+        <div>
+            {/* <InteractiveWelcome /> */}
+            {/* <Login /> */}
+            {/* <UncontrolledLogin /> */}
+            <ConfigurableList subject={"groceries"} render={(state, deleteFromListHandler) => (
+                <ul>
+                    {state.map((item, index) => {
+                        if (item) {
+                            return (
+                                <li listnumber={index} key={"listItem" + index}>{item}&nbsp;
+                                    <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
+                                </li>
+                            );
+                        } else {return null}
+                    })}
+                </ul>
+            )}/>
+            <ConfigurableList subject={"todos"} render={(state, deleteFromListHandler) => (
+                <ol>
+                    {state.map((item, index) => {
+                        if (item) {
+                            return (
+                                <li listnumber={index} key={"listItem" + index}>{item}&nbsp;
+                                    <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
+                                </li>
+                            );
+                        } else {return null}
+                    })}
+                </ol>
+            )}/>
+            <ConfigurableList subject={"cats"} render={(state, deleteFromListHandler) => (
+                <div>
+                    {state.map((item, index) => {
+                        if (item) {
+                            return (
+                                <div listnumber={index} key={'cat' + index}>
+                                    <img style={catStyle} src={item} key={"listItem" + index} alt={'image number '+index}/>
+                                    <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
+                                </div>
+                            );
+                        } else {return null}
+                    })}
+                </div>
+            )}/>
+            {/* <Container title={<h1>Hello world!</h1>}>
             <p>Lorem ipsum blablabla</p>
-        </Container >
-    </div>)
+        </Container > */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/ConfigurableList.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class TodoList extends React.Component {
+    state = {
+        itemsList: []
+    }
+
+    addToListHandler = (event) => {
+        event.preventDefault();
+        this.setState({ itemsList: [...this.state.itemsList, event.target.elements[`new${this.props.subject}element`].value] })
+        event.target.reset();
+        document.querySelector(`#${this.props.subject}listAdder`).disabled = true
+    }
+
+    deleteFromListHandler = (event) => {
+        const target = Number(event.target.parentElement.attributes.listnumber.value);
+        this.setState((state) => {
+            return {
+                itemsList: state.itemsList.filter((item, index) => index !== target)
+            }
+        })
+    }
+
+    clearListHandler = () => {
+        this.setState({ itemsList: [] });
+    }
+
+    emptyInputChecker = () => {
+        if (document.querySelector(`[name = "new${this.props.subject}element"]`).value === "") {
+            document.querySelector(`#${this.props.subject}listAdder`).disabled = true
+        } else {
+            document.querySelector(`#${this.props.subject}listAdder`).disabled = false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.addToListHandler}>
+                    <input type={'text'} name={`new${this.props.subject}element`} onChange={this.emptyInputChecker} />
+                    <button id={`${this.props.subject}listAdder`} type="submit" disabled>Add to {this.props.subject} list</button>
+                    <button onClick={this.clearListHandler}>Clear {this.props.subject} list</button>
+                </form>
+
+                {this.props.render(this.state.itemsList, this.deleteFromListHandler)}
+            </div>
+        )
+    }
+}
+
+export default TodoList;
