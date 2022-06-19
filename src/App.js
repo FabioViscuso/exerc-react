addc7d31fdd4974642d1035e025815086e7f94aa
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-06-19 18:45:26 +0200

    feat: Context-01

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 /* import InteractiveWelcome from "./InteractiveWelcome"; */
-import ConfigurableList from "./ConfigurableList";
+/* import ConfigurableList from "./ConfigurableList"; */
+import LanguageContext from "./LanguageContext"
+import DisplayLanguage from "./DisplayLanguage";
 /* import Login from "./Login"; */
 /* import Container from "./Container"; */
 /* import UncontrolledLogin from "./UncontrolledLogin"; */
+import { useState } from "react";
 
 function App() {
-    const catStyle = {
-        width: '200px',
-        borderRadius: '10px'
+    const [lang, setLang] = useState('en')
+    function setLangHandler(event) {
+        setLang(event.target.value)
     }
     return (
         <div>
             {/* <InteractiveWelcome /> */}
             {/* <Login /> */}
             {/* <UncontrolledLogin /> */}
-            <ConfigurableList subject={"groceries"} render={(state, deleteFromListHandler) => (
+            <select onChange={setLangHandler}>
+                <option value="it">Italiano</option>
+                <option value="en">English</option>
+            </select>
+            <LanguageContext.Provider value={lang}>
+                <DisplayLanguage />
+            </LanguageContext.Provider>
+            {/* <ConfigurableList subject={"groceries"} render={(state, deleteFromListHandler) => (
                 <ul>
                     {state.map((item, index) => {
                         if (item) {
@@ -23,37 +33,10 @@ function App() {
                                     <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
                                 </li>
                             );
-                        } else {return null}
+                        } else { return null }
                     })}
                 </ul>
-            )}/>
-            <ConfigurableList subject={"todos"} render={(state, deleteFromListHandler) => (
-                <ol>
-                    {state.map((item, index) => {
-                        if (item) {
-                            return (
-                                <li listnumber={index} key={"listItem" + index}>{item}&nbsp;
-                                    <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
-                                </li>
-                            );
-                        } else {return null}
-                    })}
-                </ol>
-            )}/>
-            <ConfigurableList subject={"cats"} render={(state, deleteFromListHandler) => (
-                <div>
-                    {state.map((item, index) => {
-                        if (item) {
-                            return (
-                                <div listnumber={index} key={'cat' + index}>
-                                    <img style={catStyle} src={item} key={"listItem" + index} alt={'image number '+index}/>
-                                    <button key={"button" + index} onClick={deleteFromListHandler}>Delete</button>
-                                </div>
-                            );
-                        } else {return null}
-                    })}
-                </div>
-            )}/>
+            )} /> */}
             {/* <Container title={<h1>Hello world!</h1>}>
             <p>Lorem ipsum blablabla</p>
         </Container > */}
---
+++ src/DisplayLanguage.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class DisplayLanguage extends Component {
+    render() {
+        return (<h1>Ciao</h1>)
+    }
+}
