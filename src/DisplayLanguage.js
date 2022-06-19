ecfab3ab2b98a3b63b7503d005159880e5dfec59
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-06-19 19:33:13 +0200

    feat: useContext-01 v2

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,11 @@
 import { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
+const Languages = {
+    English: 'Current Language: ',
+    Italiano: 'Lingua Attuale: '
+}
 export function DisplayLanguage() {
     const language = useContext(LanguageContext)
-    return (<h1>{language}</h1>)
+    return (<h1>{Languages[language]}{language}</h1>)
 }
